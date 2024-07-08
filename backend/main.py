007ac7c0c3085318baf5282edaf62075ba9c6077
AContiSG <84248592+AContiSG@users.noreply.github.com>
2024-07-08 19:29:07 -0300

    ajustes en models + funciona modificar usuario

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 from models import db, Usuario, Pelicula, UsuarioPeliculas
 
+load_dotenv()
+
 app = Flask(__name__)
-CORS(app)
 port = 5000
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://<nombre_usuario>:<contraseña>@localhost:5432/<nombre_baseDeDatos>'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
-@app.route('/')
+@app.route("/")
 def login():
     return """<html>
               <body>
@@ -21,17 +23,17 @@ def login():
 """
 
 
-@app.route('/usuarios')
+@app.route("/usuarios")
 def obtener_usuarios():
     try:
         usuarios = Usuario.query.all()
         usuarios_data = []
         for usuario in usuarios:
             usuario_data = {
-                'id':usuario.id,
-                'nombre':usuario.nombre,
-                'edad':usuario.edad,
-                'genero':usuario.genero
+                "id": usuario.id,
+                "nombre": usuario.nombre,
+                "edad": usuario.edad,
+                "genero": usuario.genero,
             }
             usuarios_data.append(usuario_data)
         return jsonify(usuarios_data)
@@ -40,71 +42,87 @@ def obtener_usuarios():
         return jsonify({"mensaje": "No hay usuarios."})
 
 
-@app.route('/usuarios', methods=["POST"])
+@app.route("/usuarios", methods=["POST"])
 def nuevo_usuario():
     try:
         data = request.json
-        nombre = data.get('nombre')
-        edad = data.get('edad')
-        genero = data.get('genero')
+        nombre = data.get("nombre")
+        edad = data.get("edad")
+        genero = data.get("genero")
         nuevo_usuario = Usuario(nombre=nombre, edad=edad, genero=genero)
         db.session.add(nuevo_usuario)
         db.session.commit()
-        return jsonify({'usuario':{'id': nuevo_usuario.id, 'nombre': nuevo_usuario.nombre, 'edad': nuevo_usuario.edad, 'genero':nuevo_usuario.genero}})
+        return jsonify(
+            {
+                "usuario": {
+                    "id": nuevo_usuario.id,
+                    "nombre": nuevo_usuario.nombre,
+                    "edad": nuevo_usuario.edad,
+                    "genero": nuevo_usuario.genero,
+                }
+            }
+        )
     except Exception as error:
         print(error)
-        return jsonify({'message':'No se pudo crear el usuario'}), 500
+        return jsonify({"message": "No se pudo crear el usuario"}), 500
 
 
-@app.route('/usuarios/<id_usuario>')
+@app.route("/usuarios/<id_usuario>")
 def obtener_usuario(id_usuario):
     try:
         usuario = Usuario.query.get(id_usuario)
         usuario_data = {
-        'id':usuario.id,
-        'nombre':usuario.nombre,
-        'edad':usuario.edad,
-        'genero':usuario.genero
+            "id": usuario.id,
+            "nombre": usuario.nombre,
+            "edad": usuario.edad,
+            "genero": usuario.genero,
         }
         return jsonify(usuario_data)
     except:
         return jsonify({"mensaje": "El usuario no existe"})
 
 
-@app.route('/usuarios/<id_usuario>', methods=["PUT"])
+@app.route("/usuarios/<id_usuario>", methods=["PUT"])
 def modificar_usuario_id(id_usuario):
-    id = request.json.get("id")
-    nombre = request.json.get("nombre")
-    edad = request.json.get("edad")
-    genero = request.json.get("genero")
-    usuario = {
-        "id": id,
-        "nombre": nombre,
-        "edad": edad,
-        "genero": genero
-    }
-    return {"success": modificar_usuario(usuario), "id": id}
+    try:
+        usuario = db.session.query(Usuario).filter_by(id=id_usuario).one_or_none()
+        data = request.json
+
+        usuario.nombre = data.get("nombre")
+        usuario.edad = data.get("edad")
+        usuario.genero = data.get("genero")
+        db.session.commit()
+        return jsonify(
+            {
+                "id": id_usuario,
+                "nombre": data.get("nombre"),
+                "edad": data.get("edad"),
+                "genero": data.get("genero"),
+            }
+        )
+    except Exception as error:
+        print(error)
+        return jsonify({"message": "No se pudo crear el usuario"}), 500
 
 
-
-@app.route('/usuarios/<id_usuario>', methods=["DELETE"])
+@app.route("/usuarios/<id_usuario>", methods=["DELETE"])
 def eliminar_usuario_id(id_usuario):
     return {"success": eliminar_usuario(id)}
 
 
-@app.route('/peliculas')
+@app.route("/peliculas")
 def mostrar_peliculas():
     try:
         peliculas = Pelicula.query.all()
         peliculas_data = []
         for pelicula in peliculas:
             pelicula_data = {
-                'id_pelicula':pelicula.id_pelicula,
-                'nombre':pelicula.nombre,
-                'director':pelicula.director,
-                'año_estreno':pelicula.año_estreno,
-                'genero':pelicula.genero,
-                'imagen':pelicula.imagen
+                "id": pelicula.id,
+                "nombre": pelicula.nombre,
+                "director": pelicula.director,
+                "año_estreno": pelicula.año_estreno,
+                "genero": pelicula.genero,
+                "imagen": pelicula.imagen,
             }
             peliculas_data.append(pelicula_data)
         return jsonify(peliculas_data)
@@ -113,34 +131,51 @@ def mostrar_peliculas():
         return jsonify({"mensaje": "No hay peliculas."})
 
 
-@app.route('/peliculas',methods=["POST"])
+@app.route("/peliculas", methods=["POST"])
 def nueva_pelicula():
     try:
         data = request.json
-        nombre = data.get('nombre')
-        director = data.get('director')
-        año_estreno = data.get('año_estreno')
-        genero = data.get('genero')
-        imagen = data.get('imagen')
-        nueva_pelicula = Usuario(nombre=nombre, director=director, año_estreno=año_estreno, genero=genero, imagen=imagen)
+        nombre = data.get("nombre")
+        director = data.get("director")
+        año_estreno = data.get("año_estreno")
+        genero = data.get("genero")
+        imagen = data.get("imagen")
+        nueva_pelicula = Pelicula(
+            nombre=nombre,
+            director=director,
+            año_estreno=año_estreno,
+            genero=genero,
+            imagen=imagen,
+        )
         db.session.add(nueva_pelicula)
         db.session.commit()
-        return jsonify({'pelicula':{'id_pelicula': nueva_pelicula.id_pelicula, 'nombre': nueva_pelicula.nombre, 'director': nueva_pelicula.director, 'año_estreno':nueva_pelicula.año_estreno, 'genero':nueva_pelicula.genero, 'imagen':nueva_pelicula.imagen}})
+        return jsonify(
+            {
+                "pelicula": {
+                    "id": nueva_pelicula.id,
+                    "nombre": nueva_pelicula.nombre,
+                    "director": nueva_pelicula.director,
+                    "año_estreno": nueva_pelicula.año_estreno,
+                    "genero": nueva_pelicula.genero,
+                    "imagen": nueva_pelicula.imagen,
+                }
+            }
+        )
     except Exception as error:
         print(error)
-        return jsonify({'message':'No se pudo crear la pelicula'}), 500
+        return jsonify({"message": "No se pudo crear la pelicula"}), 500
 
 
-@app.route('/peliculas/<id_pelicula>')
+@app.route("/peliculas/<id_pelicula>")
 def obtener_pelicula(id_pelicula):
     try:
         pelicula = Pelicula.query.get(id_pelicula)
         pelicula_data = {
-        'id_pelicula':pelicula.id_pelicula,
-        'nombre':pelicula.nombre,
-        'director':pelicula.director,
-        'año_estreno':pelicula.año_estreno,
-        'imagen':pelicula.imagen
+            "id": pelicula.id,
+            "nombre": pelicula.nombre,
+            "director": pelicula.director,
+            "año_estreno": pelicula.año_estreno,
+            "imagen": pelicula.imagen,
         }
         return jsonify(pelicula_data)
     except Exception as error:
@@ -148,33 +183,30 @@ def obtener_pelicula(id_pelicula):
         return jsonify({"mensaje": "La pelicula no existe"})
 
 
-@app.route('/peliculas/id_pelicula', methods=["PUT"])
-def modificar_pelicula():
-    id_pelicula = request.json.get("id_pelicula")
+@app.route("/peliculas/<id_pelicula>", methods=["PUT"])
+def modificar_pelicula(id_pelicula):
     nombre = request.json.get("nombre")
     director = request.json.get("director")
     año_estreno = request.json.get("año_estreno")
     genero = request.json.get("genero")
     imagen = request.json.get("imagen")
     pelicula = {
-        "id_pelicula": id_pelicula,
         "nombre": nombre,
         "director": director,
         "año_estreno": año_estreno,
         "genero": genero,
-        "imagen": imagen
+        "imagen": imagen,
     }
-    return {"success": modificar_usuario(pelicula), "id_pelicula": id_pelicula}
+    return {"success": modificar_usuario(pelicula), "id": id}
 
 
-@app.route('/peliculas/<id_pelicula>', methods=["DELETE"])
+@app.route("/peliculas/<id_pelicula>", methods=["DELETE"])
 def eliminar_pelicula_id(id_pelicula):
     return {"success": eliminar_pelicula(id)}
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', debug=True, port=port)
+    app.run(host="0.0.0.0", debug=True, port=port)
--- backend/models.py
+++ backend/models.py
@@ -2,28 +2,30 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Usuario(db.Model):
-    __tablename__ = 'usuarios'
+    __tablename__ = "usuarios"
     id = db.Column(db.Integer, primary_key=True)
+    id_usuario_peliculas = db.Column(db.Integer, db.ForeignKey("usuario_peliculas.id"))
     nombre = db.Column(db.String(50), nullable=False)
     edad = db.Column(db.Integer, nullable=False)
     genero = db.Column(db.String(50), nullable=False)
 
+
 class Pelicula(db.Model):
-    __tablename__ = 'peliculas'
-    id_pelicula = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "peliculas"
+    id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50), nullable=False)
     director = db.Column(db.String(50), nullable=False)
     año_estreno = db.Column(db.Integer, nullable=False)
     genero = db.Column(db.String(50), nullable=False)
-    imagen = db.Column(db.String(50), nullable=False)
+    imagen = db.Column(db.String(200), nullable=False)
+
 
 class UsuarioPeliculas(db.Model):
-    __tablename__ = 'usuario_peliculas'
-    id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key=True)
-    id_pelicula = db.Column(db.Integer, db.ForeignKey('peliculas.id_pelicula'), primary_key=True)
-    score = db.Column(db.Integer, nullable=False)
-    usuario = db.relationship('Usuario', backref=db.backref('usuario_peliculas', cascade='all, delete-orphan'))
-    pelicula = db.relationship('Pelicula', backref=db.backref('usuario_peliculas', cascade='all, delete-orphan'))
-
-
+    __tablename__ = "usuario_peliculas"
+    id = db.Column(db.Integer, primary_key=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=False)
+    id_pelicula = db.Column(db.Integer, db.ForeignKey("peliculas.id"), nullable=False)
+    puntaje = db.Column(db.Integer)
+    reseña = db.Column(db.String(200))
