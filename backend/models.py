2ae3ac4b176a39d36db8a9efbb6e7a40e490437e
AContiSG <84248592+AContiSG@users.noreply.github.com>
2024-07-27 13:49:53 -0300

    obtener reseñas de un usuario

--- backend/models.py
+++ backend/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class Usuario(db.Model):
     __tablename__ = "usuarios"
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50), nullable=False, unique=True)
+    nombre = db.Column(db.String(100), nullable=False, unique=True)
     edad = db.Column(db.Integer, nullable=False)
     genero = db.Column(db.String(50), nullable=False)
     peliculas = db.relationship(
@@ -17,11 +17,11 @@ class Usuario(db.Model):
 class Pelicula(db.Model):
     __tablename__ = "peliculas"
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50), nullable=False, unique=True)
-    director = db.Column(db.String(50), nullable=False)
+    nombre = db.Column(db.String(300), nullable=False, unique=True)
+    director = db.Column(db.String(100), nullable=False)
     año_estreno = db.Column(db.Integer, nullable=False)
-    genero = db.Column(db.String(50), nullable=False)
-    imagen = db.Column(db.String(30000), nullable=False)
+    genero = db.Column(db.String(100), nullable=False)
+    imagen = db.Column(db.String(2000), nullable=False)
     usuarios = db.relationship(
         "Reseña", backref="pelicula", cascade="all, delete-orphan"
     )
@@ -33,4 +33,4 @@ class Reseña(db.Model):
     id_usuario = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=False)
     id_pelicula = db.Column(db.Integer, db.ForeignKey("peliculas.id"), nullable=False)
     puntaje = db.Column(db.Integer)
-    reseña_corta = db.Column(db.String(500))
+    reseña_corta = db.Column(db.String(1000))
